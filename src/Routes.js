4102dc12f107679cf4a6d59eed1568a1308a9d4d
Sneha Shaw <snehashaw122@gmail.com>
2022-11-17 11:19:42 +0530

    `feat`: `home`

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
 const Router = () => {
 
     return (
             <BrowserRouter>
                 <Routes>
+                    <Route path="" element={<Home />} />
                 </Routes>
             </BrowserRouter>
 

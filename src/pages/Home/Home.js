82b285f96fbf1dd315d18ca8c3e72218de8136b0
Sneha Shaw <snehashaw122@gmail.com>
2022-11-17 12:02:09 +0530

    `fix`: `home`

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './styles.css'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   // check user data 
   // if user is logged in, show user data
   // if user is not logged in, show login/register buttons
   const user = null
+  const navigate = useNavigate()
   return (
     <div className='container'>
       {user ? (
@@ -20,8 +22,12 @@ const Home = () => {
           {/* <h1>Home</h1> */}
           <h2>Welcome <span className='name'>Guest,</span>
             </h2>
-          <button>Login</button>
-          <button>Register</button>
+          <button
+            onClick={() => navigate('/login')}
+          >Login</button>
+          <button
+            onClick={() => navigate('/register')}
+          >Register</button>
           </div>
       )}
     </div>

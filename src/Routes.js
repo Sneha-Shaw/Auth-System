a43375e68e761b35475e5df1caa4f22e75c2df81
Sneha Shaw <snehashaw122@gmail.com>
2022-11-17 12:00:50 +0530

    `feat`: pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+
 const Router = () => {
 
     return (
             <BrowserRouter>
                 <Routes>
                     <Route path="" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
             </BrowserRouter>
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import './styles.css'
 
 const Home = () => {
+  // check user data 
+  // if user is logged in, show user data
+  // if user is not logged in, show login/register buttons
+  const user = null
   return (
-    <div>Home</div>
+    <div className='container'>
+      {user ? (
+        <div className='user'>
+          {/* <h1>Home</h1> */}
+          <h2>Welcome <span className='name'>{user.name},</span>
+             {/* {user.name} */}
+             </h2>
+          </div>
+      ) : (
+        <div className='guest'>
+          {/* <h1>Home</h1> */}
+          <h2>Welcome <span className='name'>Guest,</span>
+            </h2>
+          <button>Login</button>
+          <button>Register</button>
+          </div>
+      )}
+    </div>
   )
 }
 
